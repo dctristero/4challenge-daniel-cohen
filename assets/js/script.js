f9bce6ccdb3e7185b2b58dc655920d38db2ec878
dctristero <123331986+dctristero@users.noreply.github.com>
2023-03-08 16:01:15 -0600

    show/hide, endgame, deducting time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
 var startBtn = document.querySelector("#start");
-var timeEl = document.querySelector(".time");
+var timeEl = document.getElementById("time");
 var mainEl = document.getElementById("main");
+var initEl = document.getElementById("init");
 var qArea = document.getElementById("Q-area");
-var AArea = document.getElementById("A-area");
-var secondsLeft = 70;
+var aArea = document.getElementById("A-area");
+var secondsLeft = 10;
 var a1Btn = document.getElementById("a1");
 var a2Btn = document.getElementById("a2");
 var a3Btn = document.getElementById("a3");
 var a4Btn = document.getElementById("a4");
+var form = document.getElementById("form");
 var aBtn = document.querySelectorAll(".A-btn");
+var feedback = document.getElementById("feedback");
+var feedbackExpiration;
+var message = document.getElementById("message");
+
 
 // object full of objects for Qs and corresponding A arrays
 
@@ -32,63 +38,151 @@ var quizChunks = [
   ans2: "rats",
   ans3: "food",
   ans4: "all of the above",
-  correct: "all of the above"}
+  correct: "all of the above"},
+
+  {question: "who deserves kindness",
+  ans1: "steve",
+  ans2: "everyone but steve",
+  ans3: "hugh grant",
+  ans4: "hugh grant",
+  correct: "everyone but steve"},
+
+  {question: "why am i sad",
+  ans1: "because of the horrors",
+  ans2: "pizza",
+  ans3: "rats", 
+  ans4: "hugh grant",
+  correct: "because of the horrors"},
 ];
+
+var current = 0;
+var Q = quizChunks[current].question;
+var A1 = quizChunks[current].ans1;
+var A2 = quizChunks[current].ans2;
+var A3 = quizChunks[current].ans3;
+var A4 = quizChunks[current].ans4;
+
+showInit();
+
+
+function showInit() {
+  initEl.classList.toggle("hide");
+}
 
 function setTime() {
   // Sets interval in variable
   var timerInterval = setInterval(function() {
     secondsLeft--;
     timeEl.textContent = secondsLeft;
-
     if(secondsLeft === 0) {
       // Stops execution of action at set interval
       clearInterval(timerInterval);
+      endQuiz();
       // Calls function to create and append image
-      sendMessage();
+      // sendMessage();
     }
 
   }, 1000);
 }
 // Function for message at end of timer
-function sendMessage() {
-  timeEl.textContent = "0";
-// instead of img, append item to enter initials/score
+// function sendMessage() {
+//   timeEl.textContent = "0";
+// // instead of img, append item to enter initials/score
 
-//   var imgEl = document.createElement("img");
-//   imgEl.setAttribute("src", "images/image_1.jpg");
-//   mainEl.appendChild(imgEl);
-}
+// //   var imgEl = document.createElement("img");
+// //   imgEl.setAttribute("src", "images/image_1.jpg");
+// //   mainEl.appendChild(imgEl);
+// }
+
 startBtn.addEventListener("click", function() {
     setTime();
-    startQuiz();
+    showQuiz();
 });
 
-function startQuiz() {
-    var current = 0;
+
+function showQuiz() {
+  timeEl.classList.toggle("hide");
+  timeEl.textContent = secondsLeft;
+  initEl.classList.toggle( 'hide' );
+  qArea.classList.toggle( 'hide' );
+  aArea.classList.toggle( 'hide' );
+  qArea.innerHTML = Q;
+  a1Btn.innerHTML = A1;
+  a2Btn.innerHTML = A2;
+  a3Btn.innerHTML = A3;
+  a4Btn.innerHTML = A4;
+}
+
+
+
+aArea.addEventListener("click", check);
+
+
+function check(event) {
+  var right = quizChunks[current].correct;
+  
+  if (event.target.textContent == right) {
+    next();
+    feedback.innerHTML = "good doggy";
+    feedbackExpiration = setTimeout(function () {
+      feedback.innerHTML = "";
+    }, 1000);
+  }
+  else {
+    punish(10);
+    next();
+    feedback.innerHTML = "how dare you";
+    feedbackExpiration = setTimeout(function () {
+      feedback.innerHTML = "";
+    }, 1000);
+    stopAtZero();
+    timeEl.textContent = secondsLeft;
+  };
+}
+
+function stopAtZero() {
+  if (secondsLeft <= 0) {
+    secondsLeft = 0;
+  endQuiz();
+}}
+
+function punish(seconds) {
+  secondsLeft -= seconds;
+}
+
+function hideFeedback() {
+  clearTimeout(feedbackExpiration);
+}
+
+
+function next() {
+  current++;
+  hideFeedback();
+
+  if (current < 5) {
     var Q = quizChunks[current].question;
     var A1 = quizChunks[current].ans1;
     var A2 = quizChunks[current].ans2;
     var A3 = quizChunks[current].ans3;
     var A4 = quizChunks[current].ans4;
-    
-    qArea.innerHTML = "<p>" + Q + "</p>";
+ 
+    qArea.innerHTML = Q;
     a1Btn.innerHTML = A1;
     a2Btn.innerHTML = A2;
     a3Btn.innerHTML = A3;
     a4Btn.innerHTML = A4;
-};
-
-AArea.addEventListener("click", check);
-
-function check(event) {
-  var current = 0;
-  var right = quizChunks[current].correct;
-  if (event.target.textContent == right) {
-    qArea.innerHTML = "<p>ksghfuizgfksjfgsjkfgsukfgykugwfkaygfrakuegyweakgrywekauyeg</p>";
   }
-  else {
-    qArea.innerHTML = "<p>KKKKKKKKK</p>";
-  };
-};
 
+  else {
+    endQuiz();
+  }
+}
+
+function endQuiz() {
+  qArea.classList.toggle("hide");
+  aArea.classList.toggle("hide");
+  form.classList.toggle("hide");
+  timeEl.classList.toggle("hide");
+  message.textContent = 
+  "I'm so proud of you just for existing. Your score is " + secondsLeft + "."
+}
